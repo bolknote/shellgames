16a93c74fa83ddb4b98ea40c01196bd2be73dd3d
Евгений Степанищев <Evgeny.Stepanischev@tatar.ru>
2012-08-24 23:51:55 +0400

    Приблуда для удаления раскладки U.S. English

---
+++ us_layout_remover.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Evgeny Stepanischev Aug 2012 http://bolknote.ru
+# http://artpolikarpov.ru/2012/08/24/1/
+
+PLIST=~/Library/Preferences/ByHost/com.apple.HIToolbox.*.plist
+BACKUP=$(echo ~/Library/Preferences/ByHost/com.apple.HIToolbox.*.plist).backup
+
+function GetSection {
+    /usr/libexec/PlistBuddy -c "Print :$1" $PLIST
+}
+
+function WhereUS {
+    local num=0
+
+    GetSection "$1" |
+    while read -r -d} line; do
+        if echo "$line" | grep -Fq U.S.; then
+            echo $num
+            break
+        fi
+
+        let 'num++'
+    done
+}
+
+function DeleteFromSection {
+    [ -n "$2" ] || return
+
+    /usr/libexec/PlistBuddy -c "Delete :$1:$2 dict" $PLIST
+}
+
+# Смотрим, не было ли бакапа
+if [ -e $BACKUP ]; then
+    echo Please remove backup file first:
+    echo $BACKUP
+    exit 1
+fi
+
+# если не было, сохраняем исходный файл
+cp $PLIST $BACKUP
+
+# удаляем упоминание о раскладке US изо всех секций
+/usr/libexec/PlistBuddy -c Print $PLIST | awk '/Array {/ {print $1}' |
+while read field; do
+     DeleteFromSection $field `WhereUS $field`
+done
+
+osascript -e 'tell application "System Events" to log out'
